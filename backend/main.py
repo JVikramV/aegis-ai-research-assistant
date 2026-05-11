a0606396cd2f73592334a6194492b969d9352c92
V Jayanth Vikram <jayanthshikar@Vs-MacBook-Air.local>
2026-05-11 22:12:15 +0530

    Updated production CORS

--- backend/main.py
+++ backend/main.py
@@ -22,7 +22,7 @@ app.mount(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://aegis-ai-research-assistant.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
